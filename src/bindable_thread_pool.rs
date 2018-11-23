af0b261897f02a21768c1cf8f97686774935774f
Saurabh Raje <sraje@yeti-4.grenoble.grid5000.fr>
2018-11-23 14:43:56 +0100

    Debugging added

--- src/bindable_thread_pool.rs
+++ src/bindable_thread_pool.rs
@@ -103,6 +103,7 @@ fn bind_main_thread(topo: &Mutex<Topology>) {
 }
 
 fn bind_numa(thread_id: usize, topo: &Mutex<Topology>) {
+	println!("I will bind as per ROUND_ROBIN_NUMA");
     let pthread_id = get_thread_id();
     let mut locked_topo = topo.lock().unwrap();
     let num_numa_nodes = (locked_topo)
@@ -118,6 +119,7 @@ fn bind_numa(thread_id: usize, topo: &Mutex<Topology>) {
         let my_numa_node = all_numa_nodes.get(my_numa_node_index).unwrap();
         my_numa_node.cpuset().unwrap()
     };
+    println!("want to bind to {:?}", my_core);
     my_core.singlify(); //This would give you "some" cpu node but you don't know which one.
     locked_topo
         .set_cpubind_for_thread(pthread_id, my_core, CPUBIND_THREAD)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 mod bindable_thread_pool;
 use bindable_thread_pool::POLICY;
 use bindable_thread_pool::*;
+pub fn fibonacci_recursive(n: i32) -> u64 {
+	if n < 0 {
+		panic!("{} is negative!", n);
+	}
+	match n {
+		0     => panic!("zero is not a right argument to fibonacci_reccursive()!"),
+		1 | 2 => 1,
+		3     => 2,
+		/*
+		50    => 12586269025,
+		*/
+		_     => fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+	}
+}
 fn main() {
-    BindableThreadPool::new(POLICY::ROUND_ROBIN_CORE)
-        .num_threads(2)
-        .build_global()
+let mypool =
+    BindableThreadPool::new(POLICY::ROUND_ROBIN_NUMA)
+        .num_threads(16)
+        .build()
         .expect("Thread pool build failed");
+	mypool.install(|| fibonacci_recursive(50));
     println!("Hello, world!");
 }
